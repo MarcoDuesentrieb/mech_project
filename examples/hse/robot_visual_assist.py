cf2d2be62395896d14e98bda1a47ea96a10a6310
Jonas <jonas.borkowski@gmx.de>
2025-04-16 14:10:22 +0200

    align depthframe to colorframe

--- examples/hse/robot_visual_assist.py
+++ examples/hse/robot_visual_assist.py
@@ -97,8 +97,14 @@ def main():
         while True:
             # Wait for a coherent pair of frames: depth and color
             frames = pipeline.wait_for_frames()
-            depth_frame = frames.get_depth_frame()
-            color_frame = frames.get_color_frame()
+            
+            # Align depth frame to color frame
+            align = rs.align(rs.stream.color)
+            aligned_frames = align.process(frames)
+
+            # Extract aligned depth and color frames
+            depth_frame = aligned_frames.get_depth_frame()
+            color_frame = aligned_frames.get_color_frame()
 
             if not depth_frame or not color_frame:
                 continue
