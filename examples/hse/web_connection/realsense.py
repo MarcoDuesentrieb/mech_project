c7bd63fc689d02f99e0dede06fd25f5652b04e3a
Jetson <lahowt00@hs-esslingen.de>
2025-04-23 15:09:51 +0200

    Realsense Overlay Webstream

--- examples/hse/web_connection/realsense.py
+++ examples/hse/web_connection/realsense.py
@@ -27,6 +27,7 @@ pcs = set()                             # zur Speicherung aller aktiven WebRTC-V
 # RealSense Setup
 pipeline = rs.pipeline()
 config = rs.config()
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 pipeline.start(config)
 
@@ -40,12 +41,55 @@ class RealSenseVideoStreamTrack(VideoStreamTrack):
     async def recv(self):
         pts, time_base = await self.next_timestamp()
         frames = pipeline.wait_for_frames()
-        color_frame = frames.get_color_frame()
+        
+        # Align depth frame to color frame
+        align_to = rs.stream.color
+        align = rs.align(align_to)
+        aligned_frames = align.process(frames)
+
+        depth_frame = aligned_frames.get_depth_frame()
+        depth_image = np.asanyarray(depth_frame.get_data())
+
+        color_frame = aligned_frames.get_color_frame()
         color_image = np.asanyarray(color_frame.get_data())
 
+        # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.1), cv2.COLORMAP_AUTUMN)
+
+        depth_colormap_dim = depth_colormap.shape
+        color_colormap_dim = color_image.shape
+
+        # Generate pointcloud
+        mapped_frame, color_source = color_frame, color_image
+        pc = rs.pointcloud()
+        pc.map_to(mapped_frame)
+        points = pc.calculate(depth_frame)
+        v, t = points.get_vertices(), points.get_texture_coordinates()
+        verts = np.asanyarray(v).view(np.float32).reshape(-1, 3)  # xyz
+
+        # 3D-obstacle mask
+        x_values = verts[:, 0]  # x = width
+        z_values = verts[:, 2]  # z = depth
+        y_values = verts[:, 1]  # y = height refering to axis through cameralens
+        depth_threshold = 2.0  # sets distance in meters to detect obstacle
+
+        obstacle_mask_3d = (z_values > 0) & (z_values < depth_threshold) & (y_values < 0.3) & (y_values > -0.2) & (x_values > -0.3) & (x_values < 0.3)
+        # Resize mask to match colored image
+        obstacle_mask_img = obstacle_mask_3d.reshape(depth_image.shape)
+        color_mask = np.zeros_like(color_image)
+        color_mask[obstacle_mask_img] = depth_colormap[obstacle_mask_img]
+        overlay = cv2.addWeighted(color_image, 0.7, color_mask, 0.3, 0)
+
+        # If depth and color resolutions are different, resize color image to match depth image for display
+        if depth_colormap_dim != color_colormap_dim:
+            resized_color_image = cv2.resize(color_image, dsize=(depth_colormap_dim[1], depth_colormap_dim[0]), interpolation=cv2.INTER_AREA)
+            img = np.hstack((resized_color_image, depth_colormap, overlay))
+        else:
+            img = np.hstack((color_image, depth_colormap, overlay))
+
         # Umwandlung für WebRTC
-        color_image = cv2.cvtColor(color_image, cv2.COLOR_BGR2RGB)
-        frame = VideoFrame.from_ndarray(color_image, format="rgb24")
+        img = cv2.cvtColor(overlay, cv2.COLOR_BGR2RGB)
+        frame = VideoFrame.from_ndarray(img, format="rgb24")
 
         frame.pts = pts
         frame.time_base = time_base
